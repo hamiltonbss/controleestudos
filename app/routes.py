bd8797ba6b5b117dc51b02a7910edd14d3ad3fb3
hamiltonbss <83939971+hamiltonbss@users.noreply.github.com>
2025-04-25 23:26:20 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,21 @@
-from flask import request, jsonify, current_app as app
-from .models import db, Concurso, Disciplina, Topico, SessaoEstudo
-from datetime import datetime
+from flask import render_template, request
+from .models import Concurso, Disciplina, SessaoEstudo
+from .services import gerar_cronograma, calcular_progresso, sugestao_revisao
 
 @app.route('/')
-def index():
-    return jsonify({"mensagem": "API de Estudos Pessoais"})
+def home():
+    concursos = Concurso.query.all()
+    return render_template('concurso_listar.html', concursos=concursos)
 
-@app.route('/concurso', methods=['POST'])
-def cadastrar_concurso():
-    data = request.get_json()
-    concurso = Concurso(
-        nome=data['nome'],
-        banca=data['banca'],
-        data_prova=datetime.strptime(data['data_prova'], "%Y-%m-%d")
-    )
-    db.session.add(concurso)
-    db.session.commit()
-    return jsonify({"id": concurso.id}), 201
+@app.route('/cronograma/<int:concurso_id>')
+def cronograma(concurso_id):
+    concurso = Concurso.query.get_or_404(concurso_id)
+    cronograma = gerar_cronograma(concurso_id, horas_diarias=4)  # Exemplo com 4 horas de estudo diárias
+    return render_template('cronograma.html', concurso=concurso, cronograma=cronograma)
+
+@app.route('/progresso/<int:concurso_id>')
+def progresso(concurso_id):
+    progresso = calcular_progresso(concurso_id)
+    revisoes = sugestao_revisao(concurso_id)
+    disciplinas = Disciplina.query.filter_by(concurso_id=concurso_id).all()
+    return render_template('progresso.html', progresso=progresso, revisoes=revisoes, disciplinas=disciplinas)
